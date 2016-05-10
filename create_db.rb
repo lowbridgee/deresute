75317b484614d49addafc76583904edaf84ef7a5
lowbridgee <lowbridgee>
2016-05-10 13:17:08 +0900

    Remove primary key

--- create_db.rb
+++ create_db.rb
@@ -5,8 +5,8 @@ require 'sqlite3'
 
 
 #db = SQLite3::Database.new("idle.db")
-db = SQLite3::Database.new("skill.db")
-#db = SQLite3::Database.new("center.db")
+#db = SQLite3::Database.new("skill.db")
+db = SQLite3::Database.new("center.db")
 
 # 新規DB作成
 #sql = <<SQL
@@ -23,28 +23,28 @@ db = SQLite3::Database.new("skill.db")
 #SQL
 
 # Skill DB作成
-sql = <<SQL
-CREATE TABLE skills (
-  id         integer   PRIMARY KEY AUTOINCREMENT,
-  name       text      NOT NULL,
-  skill_name text      NOT NULL,
-  skill_type text      NOT NULL,
-  interval   integer   NOT NULL,
-  length     text      NOT NULL,
-　ol_life    integer
-);
-SQL
-
-# センター効果DB作成
 #sql = <<SQL
-#CREATE TABLE center_skills (
-#  id         integer   PRIMARY KEY AUTOINCREMENT,
+#CREATE TABLE skills (
+#  id         integer   PRIMARY KEY,
 #  name       text      NOT NULL,
-#  idle_type  text      NOT NULL,
 #  skill_name text      NOT NULL,
-#  status_up  text      NOT NULL,
-#  persentage integer   NOT NULL
+#  skill_type text      NOT NULL,
+#  interval   integer   NOT NULL,
+#  length     text      NOT NULL,
+#　ol_life    integer
 #);
 #SQL
+
+# センター効果DB作成
+sql = <<SQL
+CREATE TABLE center_skills (
+  id         integer   PRIMARY KEY,
+  name       text      NOT NULL,
+  idle_type  text      NOT NULL,
+  skill_name text      NOT NULL,
+  status_up  text      NOT NULL,
+  persentage integer   NOT NULL
+);
+SQL
 
 db.execute(sql)

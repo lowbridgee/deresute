59302b35105dc300bc8529e016afe1e1e93a7c15
lowbridgee <lowbridgee>
2016-05-10 18:41:33 +0900

    Integrate database file

--- create_db.rb
+++ create_db.rb
@@ -4,38 +4,40 @@ require 'rubygems'
 require 'sqlite3'
 
 
-#db = SQLite3::Database.new("idle.db")
-#db = SQLite3::Database.new("skill.db")
-db = SQLite3::Database.new("center.db")
+db = SQLite3::Database.new("idle.db")
 
-# 新規DB作成
-#sql = <<SQL
-#CREATE TABLE idles (
-#  id         integer   PRIMARY KEY AUTOINCREMENT,
-#  idle_type  text      NOT NULL,
-#  rarelity   text    NOT NULL,
-#  name       text      NOT NULL,
-#  life       integer   NOT NULL,
-#  vocal      integer   NOT NULL,
-#  dance      integer   NOT NULL,
-#  visual     integer   NOT NULL,
-#  total      integer   NOT NULL );
-#SQL
+# アイドルテーブル作成
+sql = <<SQL
+CREATE TABLE idles (
+  id         integer   PRIMARY KEY AUTOINCREMENT,
+  idle_type  text      NOT NULL,
+  rarelity   text    NOT NULL,
+  name       text      NOT NULL,
+  life       integer   NOT NULL,
+  vocal      integer   NOT NULL,
+  dance      integer   NOT NULL,
+  visual     integer   NOT NULL,
+  total      integer   NOT NULL );
+SQL
 
-# Skill DB作成
-#sql = <<SQL
-#CREATE TABLE skills (
-#  id         integer   PRIMARY KEY,
-#  name       text      NOT NULL,
-#  skill_name text      NOT NULL,
-#  skill_type text      NOT NULL,
-#  interval   integer   NOT NULL,
-#  length     text      NOT NULL,
-#　ol_life    integer
-#);
-#SQL
+#db.execute(sql)
 
-# センター効果DB作成
+# スキル効果テーブル作成
+sql = <<SQL
+CREATE TABLE skills (
+  id         integer   PRIMARY KEY,
+  name       text      NOT NULL,
+  skill_name text      NOT NULL,
+  skill_type text      NOT NULL,
+  interval   integer   NOT NULL,
+  length     text      NOT NULL,
+　ol_life    integer
+);
+SQL
+
+db.execute(sql)
+
+# センター効果テーブル作成
 sql = <<SQL
 CREATE TABLE centers (
   id         integer   PRIMARY KEY,
@@ -46,4 +48,4 @@ CREATE TABLE centers (
 );
 SQL
 
-db.execute(sql)
+#db.execute(sql)

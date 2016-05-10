02970b6c6eed8ea47db0d3f8478f4e7d89529036
lowbridgee <lowbridgee>
2016-05-10 10:06:09 +0900

    Add center skills database

--- create_db.rb
+++ create_db.rb
@@ -6,6 +6,7 @@ require 'sqlite3'
 
 #db = SQLite3::Database.new("idle.db")
 db = SQLite3::Database.new("skill.db")
+#db = SQLite3::Database.new("center.db")
 
 # 新規DB作成
 #sql = <<SQL
@@ -21,20 +22,29 @@ db = SQLite3::Database.new("skill.db")
 #  total      integer   NOT NULL );
 #SQL
 
+# Skill DB作成
 sql = <<SQL
 CREATE TABLE skills (
   id         integer   PRIMARY KEY AUTOINCREMENT,
   name       text      NOT NULL,
-  skill      text      NOT NULL,
-  type       text      NOT NULL,
+  skill_name text      NOT NULL,
+  skill_type text      NOT NULL,
   interval   integer   NOT NULL,
   length     text      NOT NULL,
 　ol_life    integer
 );
 SQL
 
-
+# センター効果DB作成
+#sql = <<SQL
+#CREATE TABLE center_skills (
+#  id         integer   PRIMARY KEY AUTOINCREMENT,
+#  name       text      NOT NULL,
+#  idle_type  text      NOT NULL,
+#  skill_name text      NOT NULL,
+#  status_up  text      NOT NULL,
+#  persentage integer   NOT NULL
+#);
+#SQL
 
 db.execute(sql)
-
-db.close
